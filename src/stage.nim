eb1c02c813cd844ae8cd63bc98b66fa04add7dfc
bung87 <crc32@qq.com>
2020-06-08 20:11:27 +0800

    init

--- src/stage.nim
+++ src/stage.nim
@@ -1,7 +1,52 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+{.define(shellNoDebugOutput).}
+import shell
+import strutils
+import strformat
+import osproc
+import os
+import sequtils
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+proc getStagedFiles*(): seq[string] =
+  var stdout = shellVerbose:
+    git diff "--cached""--name-only""--diff-filter=d"
+  result = stdout.output.splitLines().filterIt(fileExists(it))
+
+proc checkError*(files: seq[string]): int =
+  for file in files.filterIt(it.endsWith(".nim")):
+    let (output, exitCode) = execCmdEx("nim " & "check --hints:off --colors:on  " & file)
+    if exitCode != 0:
+      result = exitCode
+    stdout.write(output)
+
+proc checkStyle*(files: seq[string]) =
+  # not used
+  # https://github.com/nim-lang/Nim/blob/27b081d1f77604ee47c886e69dbc52f53ea3741f/doc/nimfix.rst#L18
+  # --overwriteFiles:on|off overwrite the original nim files. DEFAULT is ON!
+  # --wholeProject overwrite every processed file.
+  # --checkExtern:on|off style check also extern names
+  # --styleCheck:on|off|auto performs style checking for identifiers
+
+  for file in files.filterIt(it.endsWith(".nim")):
+    let (output, exitCode) = execCmdEx("nimfix " & "--styleCheck:error --hints:off --colors:on  " & file)
+    stdout.write(output)
+
+proc fixStyle*(files: seq[string]): int =
+  for file in files.filterIt(it.endsWith(".nim")):
+    let (output, exitCode) = execCmdEx("nimpretty " & "indent:2 --maxLineLen:120 " & file)
+    if exitCode != 0:
+      result = exitCode
+    stdout.write(output)
+
+when isMainModule and defined(debug):
+  let files = getStagedFiles()
+  echo files
+  discard checkError(files)
+  discard fixStyle(files)
+
+when isMainModule and defined(release):
+  import cligen
+  proc fixStyleD(): int =
+    result = fixStyle(getStagedFiles())
+  proc checkErrorD(): int =
+    result = checkError(getStagedFiles())
+  dispatchMulti([fixStyleD, cmdName = "fixStyle"], [checkErrorD, cmdName = "checkError"])
--- stage.nimble
+++ stage.nimble
@@ -5,9 +5,21 @@ author        = "bung87"
 description   = "A new awesome nimble package"
 license       = "MIT"
 srcDir        = "src"
-
+bin = @["stage"]
 
 
 # Dependencies
 
 requires "nim >= 1.3.3"
+requires "shell"
+requires "cligen"
+
+let sh = """
+cat << EOF > .git/hooks/pre-commit
+#!/bin/sh
+stage checkError
+stage fixStyle
+EOF
+chmod +x .git/hooks/pre-commit
+"""
+exec sh
